81a06f2bf1efcc6c85c7cff679780ce976ff0390
AviderMin <gms2917466108@gmail.com>
2025-11-03 11:33:09 +0800

    clean up

--- device.mk
+++ device.mk
@@ -21,6 +21,34 @@ TARGET_SCREEN_WIDTH := 1600
 # Virtual A/B OTA
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 
+# ==================================================
+# == A/B and Postinstall ==
+# ==================================================
+PRODUCT_PACKAGES += \
+    android.hardware.boot@1.1-impl-qti \
+    android.hardware.boot@1.1-impl-qti.recovery \
+    android.hardware.boot@1.1-service \
+    bootctrl.$(TARGET_BOARD_PLATFORM)\
+    bootctrl.$(TARGET_BOARD_PLATFORM).recovery \
+    libgptutils \
+    libz \
+    libcutils
+
+PRODUCT_PACKAGES_DEBUG += \
+    bootctl
+
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_system=true \
+    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
+    FILESYSTEM_TYPE_system=ext4 \
+    POSTINSTALL_OPTIONAL_system=true
+
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_vendor=true \
+    POSTINSTALL_PATH_vendor=bin/checkpoint_gc \
+    FILESYSTEM_TYPE_vendor=ext4 \
+    POSTINSTALL_OPTIONAL_vendor=true
+
 # Update engine
 PRODUCT_PACKAGES += \
     checkpoint_gc \
